992228cfbf1e322d33bff7f5c41fd68723c3c079
Camilo Andrés Carranza <ccarranzac@unal.edu.co>
2022-05-24 14:19:57 -0500

    vue first cli app

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li>Phone: <strong>{{ friend.phone }}</strong></li>
+            <li>Email: <strong>{{ friend.email }}</strong></li>
+        </ul>
+    </li>    
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            detailsAreVisible: false,
+            friend: {
+                id: 'manuel',
+                name: 'Manuel',
+                phone: '42424',
+                email:'manuel@email.com'
+            }
+        }
+    },
+    methods:{
+        toggleDetails(){
+            this.detailsAreVisible=!this.detailsAreVisible
+        }
+    } 
+}
+</script>

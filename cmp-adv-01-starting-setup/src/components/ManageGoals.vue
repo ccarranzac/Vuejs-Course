10df32adce429745eb4b71af3e319786fc5f9a22
Camilo Andrés Carranza <ccarranzac@slabcode.com>
2022-06-01 15:55:56 -0500

    dynamic components

---
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -0,0 +1,35 @@
+<template>
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal"/>
+  <button @click="setGoal">Set goal</button>
+  <teleport to="body">
+    <error-alert v-if="inputInvalid">
+      <h2>Input is invalid</h2>
+      <p>Please enter some text!</p>
+      <button @click="confirmError">Okay</button>
+    </error-alert>
+  </teleport>
+</template>
+
+
+<script>
+import ErrorAlert from './ErrorAlert.vue'
+
+export default {
+  components:{ErrorAlert},
+  data(){
+    return{
+      inputInvalid: false,
+    }
+  },
+  methods:{
+    setGoal(){
+      const enteredValue = this.$refs.goal.value
+      if(enteredValue === ''){
+        this.inputInvalid=true
+      }
+    },
+    confirmError(){ this.inputInvalid=false }
+  }
+}
+</script>

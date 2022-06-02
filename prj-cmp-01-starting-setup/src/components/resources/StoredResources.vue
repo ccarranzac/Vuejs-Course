d305639b0005923ec2b8f5663b9d7c1c0422db2a
Camilo Andrés Carranza <ccarranzac@unal.edu.co>
2022-06-02 14:09:52 -0500

    learning resources app part 1

---
+++ prj-cmp-01-starting-setup/src/components/resources/StoredResources.vue
@@ -0,0 +1,30 @@
+<template>
+    <ul>
+        <resource-item 
+            v-for="item in resources" 
+            :key="item.id"
+            :resource="item"
+        ></resource-item>
+    </ul>
+</template>
+
+<script>
+import ResourceItem from './ResourceItem.vue'
+
+export default {
+    components:{ ResourceItem },
+    props:{ 
+        resources:{ type: Object, required: true } 
+    }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>

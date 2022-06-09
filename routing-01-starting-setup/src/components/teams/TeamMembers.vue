ab85177139672167f32f25cac151fa446d698034
Camilo Andrés Carranza <ccarranzac@unal.edu.co>
2022-06-08 20:54:21 -0500

    routes params

--- routing-01-starting-setup/src/components/teams/TeamMembers.vue
+++ routing-01-starting-setup/src/components/teams/TeamMembers.vue
@@ -9,6 +9,7 @@
         :role="member.role"
       ></user-item>
     </ul>
+    <router-link to="/teams/t2">Go to team 2</router-link>
   </section>
 </template>
 
@@ -16,17 +17,40 @@
 import UserItem from '../users/UserItem.vue';
 
 export default {
+  inject: ['users', 'teams'],
   components: {
-    UserItem
+    UserItem,
   },
   data() {
     return {
-      teamName: 'Test',
-      members: [
-        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
-        { id: 'u2', fullName: 'Max Schwarz', role: 'Engineer' },
-      ],
+      teamName: '',
+      members: [],
     };
+  },
+  watch: {
+    $route(newRoute) {
+      this.setTeamMembers(newRoute);
+    },
+  },
+  methods: {
+    setTeamMembers(route) {
+      const teamId = route.params.teamId;
+      const selectTeam = this.teams.find((team) => team.id === teamId);
+      if (!selectTeam) {
+        return;
+      }
+      const members = selectTeam.members;
+      const selectedMembers = [];
+      for (let member of members) {
+        const selectedUser = this.users.find((user) => user.id === member);
+        selectedMembers.push(selectedUser);
+      }
+      this.members = selectedMembers;
+      this.teamName = selectTeam.name;
+    },
+  },
+  created() {
+    this.setTeamMembers(this.$route);
   },
 };
 </script>
--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -2,19 +2,21 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-import TeamList from './components/teams/TeamsList.vue'
-import UserList from './components/users/UsersList.vue'
+import TeamList from './components/teams/TeamsList.vue';
+import UserList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 
-const router= createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/teams', component: TeamList },
-        { path: '/users', component: UserList }
-    ],
-    linkActiveClass: 'active'
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/teams', component: TeamList },
+    { path: '/users', component: UserList },
+    { path: '/teams/:teamId', component: TeamMembers, props: true },
+  ],
+  linkActiveClass: 'active',
 });
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 app.mount('#app');
